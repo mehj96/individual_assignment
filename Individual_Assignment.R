df6f411e5791f685e0dad7af2c170fb2cc687558
Madeline <madelineehjones@gmail.com>
2019-01-17 17:58:33 +0100

    commit

--- Individual_Assignment.R
+++ Individual_Assignment.R
@@ -20,11 +20,13 @@ ggplot() +
 
 # group total casualties (best) by year.
 
+# did NOT WORK
 ggplot() +
   geom_bar(data = Russia_entries,
              aes(x %>% 
                    group_by(year) %>% summarise(best = sum(best))))
 
+# DID WORK! created new dataset of summarized data
 Russia_total_per_year <- Russia_entries %>% group_by(year) %>% 
   summarise(best = sum(best))
 
@@ -33,13 +35,41 @@ ggplot() +
             aes(x = year, y = best))
 ggplot() +
   geom_point(data = Russia_total_per_year,
-            aes(x = year, y = best))
+            aes(x = year, y = best, size = best))
 
+# created like with RUS summary of data for USA
 USA_total_per_year <- USA_entries %>% group_by(year) %>% 
   summarise(best = sum(best))
 
 
+# Next two commands: Final version showing total casualties per year in USA and RUS
 
+ggplot() +
+  geom_point(data = USA_total_per_year,
+             aes(x = year, y = best), color = 'dark turquoise', size = 3) +
+  labs(title = "Total Casualties in the USA every Year", x = "Year", 
+       y= "Number of Casualties")
+
+ggplot() +
+  geom_point(data = Russia_total_per_year,
+             aes(x = year, y = best), color = 'dark turquoise', size = 3) +
+  labs(title = "Total Casualties in Russia every Year", x = "Year", 
+       y= "Number of Casualties")
+
+# graphs showing types of violence most common in USA and Russia.
+
+# for RUS
+ggplot() +
+  geom_bar(data = Russia_entries, 
+           aes(x = type_of_violence), color = 'red', fill = 'red') +
+  labs(title = "Types of Violence in Russia", 
+       x = "Type of Violence", y = "Occurrences")
+
+ggplot() +
+  geom_bar(data = USA_entries, 
+           aes(x = type_of_violence), color = 'red', fill = 'red') +
+  labs(title = "Types of Violence in the USA", 
+       x = "Type of Violence", y = "Occurrences")
 
 
 
